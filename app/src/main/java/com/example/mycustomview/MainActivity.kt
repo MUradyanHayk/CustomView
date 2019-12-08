dcf70b91ddcd840725f8f28ae23a01bbc6e940f4
MUradyanHayk <haykmuradyan555@gmail.com>
2019-12-08 23:44:08 +0400

    something is changed

--- app/src/main/java/com/example/mycustomview/MainActivity.kt
+++ app/src/main/java/com/example/mycustomview/MainActivity.kt
@@ -18,15 +18,16 @@ class MainActivity : AppCompatActivity() {
         val list1 = listOf("Hayk", "Hovo", "Sofya", "Harut", "Mama", "Tati")
         val list2 = listOf("24", "15", "12", "10", "44", "75")
 
-
-        val layout = MyItemLayout(this)
-        val params = FrameLayout.LayoutParams(
-            FrameLayout.LayoutParams.MATCH_PARENT,
-            FrameLayout.LayoutParams.WRAP_CONTENT
-        )
-        params.gravity = Gravity.CENTER
-        layout.layoutParams = params
-        layout.configure("First Item", "Second Item")
+//
+//        val layout = MyItemLayout(this)
+//        val params = FrameLayout.LayoutParams(
+//            FrameLayout.LayoutParams.MATCH_PARENT,
+//            FrameLayout.LayoutParams.WRAP_CONTENT
+//        )
+//        params.gravity = Gravity.CENTER
+//        layout.layoutParams = params
+//        layout.configure("First Item", "Second Item")
+//        setContentView(layout)
 
         val recycleListView: RecyclerView = findViewById(R.id.my_recycler_view)
         val adapter = MyRecyclerViewAdapter(this, list1, list2)
